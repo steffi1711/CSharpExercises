23879623324ef4994e7968b2573692f1217c88b3
Steffi Schmidt <steffi1711.schmidt@gmail.com>
2017-10-31 22:40:43 +0100

    Tag 5: Angular Persons

---
+++ Tag5Personen/AngularNgxBootstrapTemplate/AngularNgxBootstrapTemplate/app/models/person.model.ts
@@ -0,0 +1,10 @@
+export class Person{
+    id: number;
+    gender: string;
+    firstname: string;
+    lastname: string;
+    email: string;
+    country: string;
+    age: number;
+    registered: boolean;
+}
